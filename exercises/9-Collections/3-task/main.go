06e587f8d64c3d5fda6db82081c7ec3f0b0f569c
sannikovcode <sannikovbookkeeper@gmail.com>
2025-09-23 09:03:55 +0500

    collections 3-task

---
+++ exercises/9-Collections/3-task/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	temperatures := [7]int{15, 18, 12, 20, 25, 22, 16}
+	daysOfWeek := [7]string{"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"}
+
+	sum := 0
+	max := temperatures[0]
+	min := temperatures[0]
+	daysAboveAverage := 0
+
+	fmt.Println("Температура по дням неделям:")
+
+	for i, value := range temperatures {
+
+		fmt.Printf("%s : %d", daysOfWeek[i], value)
+
+		sum += value
+
+		if value > max {
+			max = value
+		}
+		if value < min {
+			min = value
+		}
+
+	}
+	result := float64(sum) / float64(7)
+
+	for _, value := range temperatures {
+		if float64(value) > result {
+			daysAboveAverage++
+		}
+
+	}
+	fmt.Println("-------------------")
+
+	fmt.Printf("Средняя температура: %.2fC", result)
+	fmt.Printf("Максимальная температура: %d\n", max)
+	fmt.Printf("Минимальная температура: %d\n", min)
+	fmt.Printf("Дней с температурой выше средней: %d\n", daysAboveAverage)
+
+}
