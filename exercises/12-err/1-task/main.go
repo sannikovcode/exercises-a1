b0237dc3465cc769af36c8ca5c74af2b38cffe4e
sannikovcode <sannikovbookkeeper@gmail.com>
2025-09-27 12:34:06 +0500

    understanding the error

---
+++ exercises/12-err/1-task/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(a, b int) (int, error) {
+
+	if b == 0 {
+
+		return 0, errors.New("деление на ноль невозможно")
+	}
+	return a / b, nil
+}
+
+func main() {
+	result, err := divide(10, 2)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
+	}
+	fmt.Println("10 / 2 =", result)
+
+	result, err = divide(10, 0)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
+	}
+	fmt.Println("Результат:", result)
+}
